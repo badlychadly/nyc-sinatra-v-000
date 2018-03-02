f2f51c323faa4dc48dc3c2c2c4fb60170a442d27
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-02 23:28:17 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,36 @@
 class FiguresController < ApplicationController
 
 
+  get '/figures' do
+    erb :'figures/index'
+  end
+
   get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params[:figure][:name])
+    @figure.title_ids = params[:figure][:title_ids]
+    @figure.landmark_ids = params[:figure][:landmark_ids]
+    @figure.titles.create(params[:title])
+    @figure.landmarks.create(params[:landmark])
+    @figure.save
+
+    redirect to("/figures/#{@figure.id}")
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
 
   end
 
